7db64eb622c7807943923b65c41a7179f2de667c
guimarczewski <guimarczewski@gmail.com>
2023-11-11 12:56:12 +0000

    create new test

--- tests.py
+++ tests.py
@@ -13,3 +13,11 @@ def test_main_status_code():
 def test_main_response():
     response = client.get("/")
     assert response.json() == {"Hello": "World"}
+
+def test_pega_um_produto():
+    response = client.get("/produtos/1")
+    assert response.json() ==     {
+        "id": 1,
+        "nome": "Notebook",
+        "preco": 3500.00,
+    }
